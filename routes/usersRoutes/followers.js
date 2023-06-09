deb49fc2bf340f7d794170e96465de78c550085d
Oscar Ivan Olivares Carsi <99694349+Carsiano@users.noreply.github.com>
2023-06-09 13:33:17 -0600

    add methods in followers

--- routes/usersRoutes/followers.js
+++ routes/usersRoutes/followers.js
@@ -5,4 +5,6 @@ router.get('/followers/:username',followersProxy);
 router.get('/following/:username',followersProxy);
 router.post('/',followersProxy);
 router.delete('/:follower/:following', followersProxy); 
+router.get('/followingUsers/:username',followersProxy);
+router.get('/followersUsers/:username',followersProxy);
 module.exports = router;
