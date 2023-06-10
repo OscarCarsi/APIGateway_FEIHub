1595fcfee333ebaca4d1a8fbf6d5760cd75bc6ad
Oscar Ivan Olivares Carsi <99694349+Carsiano@users.noreply.github.com>
2023-06-09 22:44:07 -0600

    add methods in users

--- routes/usersRoutes/users.js
+++ routes/usersRoutes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const proxyUsers = require ('./usersApiProxy')
 router.get('/:username', proxyUsers); 
+router.get('/findUsers/:username', proxyUsers); 
 router.post('/', proxyUsers);
 router.put('/:username', proxyUsers);
 router.patch('/:username', proxyUsers);
